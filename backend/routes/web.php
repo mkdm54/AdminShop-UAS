fa5691b3d8ce9b0b77ba3a3b55ffc64f92dc45a1
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-24 22:29:49 +0700

    hapus cotroller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\admin;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([AdminMiddleware::class])->group(function(){
-    Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/admin', [])->name('admin.dashboard');
 });
