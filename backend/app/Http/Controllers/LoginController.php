37665ec564ff4a9896c6766b062a6804067bd0d4
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-24 23:59:14 +0700

    Menambahkan route untuk form login dan membuat LoginController

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController
+{
+    public function showLoginForm()
+    {
+        
+    }
+
+    public function login() {
+
+    }
+}
