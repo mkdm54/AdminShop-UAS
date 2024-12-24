f7a2d15a055d555e2cd04a9c4b77bffa23137964
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-24 19:03:32 +0700

    Membuat entri pengguna admin dengan email dan password

--- backend/database/seeders/AdminSeeder.php
+++ backend/database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'makdumibrohim@gmail.com',
+            'password' => bcrypt('admin1234'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
     }
 }
