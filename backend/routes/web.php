141712234c6cb0d48d90c15901928f1394f45142
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-24 22:37:14 +0700

    Tambahkan rute dashboard untuk pengguna dengan middleware UserMiddleware

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
-use App\Http\Controllers\admin;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\USerMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([AdminMiddleware::class])->group(function(){
-    Route::get('/admin', [])->name('admin.dashboard');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+Route::middleware([USerMiddleware::class])->group(function(){
+    Route::get('/dashboard',[UserController::class, 'index'])->name('user.dashboard');
 });
