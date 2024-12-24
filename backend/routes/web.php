863b6b3cb417a1c77545d1286e1129436adcc61f
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-24 20:01:38 +0700

    Menambahkan middleware untuk rute admin

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware([AdminMiddleware::class])->group(function(){
+    Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 });
