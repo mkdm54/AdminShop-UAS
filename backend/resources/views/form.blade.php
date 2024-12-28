f47bad999aea12c27be13f439fb1e4a37786a319
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-28 10:14:07 +0700

    megatur panjang input

--- backend/resources/views/form.blade.php
+++ backend/resources/views/form.blade.php
@@ -13,20 +13,20 @@
         {{-- <div class="logo">
             <img src="logo.png" alt="Logo">
         </div> --}}
-        <form method="POST">
+        <form id="form-input" method="POST">
             @csrf
             <div class="input-group">
                 <label for="name">Username<span class="required font-mono">*</span></label>
-                <input type="text" id="name" name="name" placeholder="Enter your username" required>
+                <input class="username-input" type="text" id="name" name="name" placeholder="Enter your username" required>
             </div>
             <div class="input-group">
-                <label for="email">Email<span class="required">*</span></label>
-                <input type="email" id="email" name="email" placeholder="Enter your email address" required>
+                <label for="email">Email<span class="required font-mono" >*</span></label>
+                <input class="email-input" type="email" id="email" name="email" placeholder="Enter your email address" required>
             </div>
             <div class="input-group">
-                <label for="password">Password<span class="required">*</span></label>
+                <label for="password">Password<span class="required font-mono">*</span></label>
                 <div class="password-wrapper">
-                    <input type="password" id="password" name="password" placeholder="Enter password" required>
+                    <input class="pw-input" type="password" id="password" name="password" placeholder="Enter password" required>
                     <button type="button" class="show-password"></button>
                 </div>
             </div>
