5794b50fb6dd7e444c40e4d28e3a2fe11aecf468
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-31 09:22:26 +0700

    Ubah font di form menjadi inter

--- backend/resources/views/form.blade.php
+++ backend/resources/views/form.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite(['resources/css/form.css'])
     <title>Login Page</title>
 </head>
