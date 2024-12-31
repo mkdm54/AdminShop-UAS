0691e0418de37c854fd9ac7e2d0e5b397482cd85
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-12-31 11:50:39 +0700

    perubahan nama  user middleware

--- backend/app/Http/Middleware/USerMiddleware.php
+++ backend/app/Http/Middleware/USerMiddleware.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class USerMiddleware
+class UserMiddleware
 {
     /**
      * Handle an incoming request.
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\USerMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(LoginController::class)->group(function () {
@@ -17,6 +17,6 @@ Route::middleware([AdminMiddleware::class])->group(function () {
     Route::resource('admin', AdminController::class);
 });
 
-Route::middleware([USerMiddleware::class])->group(function () {
+Route::middleware([UserMiddleware::class])->group(function () {
     Route::resource('user', UserController::class);
 });
