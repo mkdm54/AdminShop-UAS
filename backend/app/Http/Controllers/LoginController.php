b192d74d93972363dc83f640596c9d5395510cb4
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-01 22:56:48 +0700

    Amankan sesi logout dengan invalidate() dan regenerateToken().

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -37,6 +37,8 @@ class LoginController
     public function logout(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect('/');
     }
 }
