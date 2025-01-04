2d740a3b98c34223801c9210212e6958711af3fb
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-04 20:58:13 +0700

    ubah nama kelas dari LoginController > AuthController

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(LoginController::class)->group(function () {
+Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'showLoginForm')->name('home');
     Route::post('/login', 'login')->name('login');
     Route::post('/logout', 'logout')->name('logout');
