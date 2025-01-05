887b2a205860b33b5f35217ae2925cbc7f8c433a
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-05 21:08:51 +0700

    mengubah title setiap halaman

--- backend/resources/views/auth/password/email.blade.php
+++ backend/resources/views/auth/password/email.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite(['resources/css/form.css'])
-    <title>Login Page</title>
+    <title>Forgot Password Page</title>
 </head>
 
 <body class="bg-indigo-500 flex justify-center items-center h-screen m-0">
