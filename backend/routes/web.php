59bdb91b78d64941e6c11ae477c2a1795a268d41
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-05 12:34:15 +0700

    Hapus

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
