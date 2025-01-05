4db74aa8c6ffa9731ab6e9dc7314bc5b47f64f36
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-05 22:39:23 +0700

    tambahkan wrapper

--- backend/resources/views/form.blade.php
+++ backend/resources/views/form.blade.php
@@ -11,27 +11,27 @@
 </head>
 
 <body class="bg-indigo-500 flex justify-center items-center h-screen m-0">
-    <div class="login-container bg-white p-8 rounded-lg shadow-md text-center w-96">
-        {{-- <div class="logo">
-            <img src="logo.png" alt="Logo">
-        </div> --}}
-        <form id="form-input" method="POST" action="{{ route('login') }}">
-            @csrf
-            <x-input-component name="username" label="Username" type="text" placeholder="Enter your username"></x-input-component>
-            <x-input-component name="password" label="Password" type="password" placeholder="Enter your password"></x-input-component>
-            <div class="options flex justify-between text-xs mb-4">
-                <label>
-                    <input type="checkbox" name="remember">
-                    Remember me
-                </label>
-                <a href="{{ route('password.request') }}">Forgot your password?</a>
-            </div>
-            <x-button type="submit">Log In</x-button>
-        </form>
-        <p class="register text-sm text-gray-600 mt-4">
-            Don’t have an account? <a href="{{ route('register.form') }}">Register here</a>
-        </p>
+    <div class="flex flex-col items-center gap-4">
+        <div class="login-container bg-white p-8 rounded-lg shadow-md text-center w-96">
+            <form id="form-input" method="POST" action="{{ route('login') }}">
+                @csrf
+                <x-input-component name="username" label="Username" type="text" placeholder="Enter your username"></x-input-component>
+                <x-input-component name="password" label="Password" type="password" placeholder="Enter your password"></x-input-component>
+                <div class="options flex justify-between text-xs mb-4">
+                    <label>
+                        <input type="checkbox" name="remember">
+                        Remember me
+                    </label>
+                    <a href="{{ route('password.request') }}">Forgot your password?</a>
+                </div>
+                <x-button type="submit">Log In</x-button>
+            </form>
+            <p class="register text-sm text-gray-600 mt-4">
+                Don’t have an account? <a href="{{ route('register.form') }}">Register here</a>
+            </p>
+        </div>
     </div>
 </body>
+
 
 </html>
