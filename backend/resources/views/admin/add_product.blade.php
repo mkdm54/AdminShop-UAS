f808e3c5ba78e0b2e04096f8faee441e58eaec6c
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-12 20:46:03 +0700

    ubah rute tambah produk

--- backend/resources/views/admin/add_product.blade.php
+++ backend/resources/views/admin/add_product.blade.php
@@ -60,16 +60,16 @@
                 <h1 class="w-full text-3xl text-black pb-6">Add Product</h1>
                 <div class="flex flex-col items-center gap-4">
                     <p class="text-xl pb-3 flex items-center">
-                        <i class="fas fa-list mr-3"></i> Table Example
+                        <i class="fas fa-list mr-3"></i> Product
                     </p>
                     <x-alert-message></x-alert-message>
                     <div class="login-container bg-white p-8 rounded-lg shadow-md text-center w-96">
-                        <form method="POST">
+                        <form method="POST" action="{{ route('admin.store') }}">
                             @csrf
                             <x-input-component id="product_name" name="product_name" label="Add Product" type="text" placeholder="Enter your product"></x-input-component>
-                            <x-input-component id="price" name="price" label="Price" type="number" placeholder="Enter your price"></x-input-component>
+                            <x-input-component id="price" name="price" label="Price" type="text" placeholder="Enter your price"></x-input-component>
                             <x-input-component id="description" name="description" label="Description" type="text" placeholder="Description"></x-input-component>
-                            <x-input-component id="quantity" name="quantity" label="Quantity" type="number" placeholder="Quantity"></x-input-component>
+                            <x-input-component id="quantity" name="quantity" label="Quantity" type="text" placeholder="Quantity"></x-input-component>
                             <x-button type="submit">Add</x-button>
                         </form>
                     </div>
