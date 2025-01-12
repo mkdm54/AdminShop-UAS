cea39041db7f737586404f6a7cce9fb77c81a910
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-12 21:31:43 +0700

    memperbaiki posisi icon

--- backend/resources/views/components/product-table.blade.php
+++ backend/resources/views/components/product-table.blade.php
@@ -95,19 +95,20 @@
                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
                         23/04/18</p>
                 </td>
-                <td class="p-4 border-b border-blue-gray-50">
-                    <button
-                        class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30"
-                        type="button">
-                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                aria-hidden="true" class="h-4 w-4">
-                                <path
-                                    d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z">
-                                </path>
-                            </svg>
+
+                <td class=" p-4 border-b border-blue-gray-50">
+
+                    <div
+                        class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30">
+                        <span class="relative top-3 transform -translate-y-1/2 -translate-x-1/2" title="Edit">
+                            <a href="#"><i class="fa-solid fa-pencil text-green-500 text-lg"></i></a>
                         </span>
-                    </button>
+
+                        <span class="relative top-3 left-2 transform -translate-y-1/2 -translate-x-1/2" title="Delete">
+                            <a href="#"><i class="fa-solid fa-trash text-red-500 text-lg"></i></a>
+                        </span>
+                    </div>
+
                 </td>
             </tr>
         </tbody>
