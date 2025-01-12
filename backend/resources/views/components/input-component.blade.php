f47728c2859d58628a977685a3d42a4027dbee83
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-12 17:29:04 +0700

    Memperbaiki error dependency parameter di InputComponent

--- backend/resources/views/components/input-component.blade.php
+++ backend/resources/views/components/input-component.blade.php
@@ -2,7 +2,4 @@
     <label for="{{ $name }}">{{ $label }}<span class="required text-red-500 font-mono">*</span></label>
     <input class="username-input" type="{{ $type }}" id="{{ $id}}" name="{{ $name }}"
         placeholder="{{ $placeholder }}" required>
-
-    {{-- alert --}}
-    <i class="text-xs text-red-700" hidden>{{ $error }}</i>
 </div>
