7c2f0fcc4c10318ed48a131020a2bc0ac804066a
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-12 13:16:58 +0700

    Mobile Header & Nav

---
+++ backend/resources/views/components/mobile-header-nav.blade.php
@@ -0,0 +1,16 @@
+<header x-data="{ isOpen: false }" class="w-full bg-sidebar py-5 px-6 sm:hidden">
+    <div class="flex items-center justify-between">
+        <a href="{{ route('admin.index') }}"
+            class="text-white text-3xl font-semibold uppercase hover:text-gray-300">Admin</a>
+        <button @click="isOpen = !isOpen" class="text-white text-3xl focus:outline-none">
+            <i x-show="!isOpen" class="fas fa-bars"></i>
+            <i x-show="isOpen" class="fas fa-times"></i>
+        </button>
+    </div>
+
+    {{-- *Dropdown Nav --}}
+    <x-dropdown-nav></x-dropdown-nav>
+    <!-- <button class="w-full bg-white cta-btn font-semibold py-2 mt-5 rounded-br-lg rounded-bl-lg rounded-tr-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
+        <i class="fas fa-plus mr-3"></i> New Report
+    </button> -->
+</header>
