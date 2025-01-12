c3a16b6856f0483663fa3cb87039cea9d4cdd3d8
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-12 12:44:07 +0700

    buat komponen untuk sidebar

--- backend/resources/views/admin/dashboard.blade.php
+++ backend/resources/views/admin/dashboard.blade.php
@@ -48,26 +48,8 @@
 
 <body class="bg-gray-100 font-family-karla flex">
 
-    <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
-        <div class="p-6">
-            <a href="{{ route('admin.index') }}"
-                class="text-white text-3xl font-semibold uppercase hover:text-gray-300">{{ $username }}</a>
-        </div>
-        <nav class="text-white text-base font-semibold pt-3">
-            <a href="{{ route('admin.index') }}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
-                <i class="fas fa-tachometer-alt mr-3"></i>
-                Dashboard
-            </a>
-            <a href="{{ route('admin.create') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-align-left mr-3"></i>
-                Add Product
-            </a>
-            <a href="{{ route('admin.products') }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-table mr-3"></i>
-                Show Product
-            </a>
-        </nav>
-    </aside>
+    {{-- Side Bar --}}
+    <x-admin-side-bar :username="$username"></x-admin-side-bar>
 
     <div class="w-full flex flex-col h-screen overflow-y-hidden">
         <!-- Desktop Header -->
