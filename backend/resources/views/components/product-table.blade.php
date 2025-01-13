943c57de7f51dd56bd79d4893fb64ecf0f61f308
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-13 10:18:59 +0700

    memberi border pada table product

--- backend/resources/views/components/product-table.blade.php
+++ backend/resources/views/components/product-table.blade.php
@@ -1,108 +1,81 @@
 @props(['products'])
 
-<div class="p-6  px-0">
-    <table class="mt-4 w-full min-w-max table-auto text-left">
-        <thead>
+<div class="p-6 px-0">
+    <table class="mt-4 w-full min-w-max table-auto text-left border">
+        <thead class="bg-blue-500">
             <tr>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Product <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                            stroke="currentColor" aria-hidden="true" class="h-4 w-4">
-                        </svg>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Product
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4"></svg>
                     </p>
                 </th>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Price <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4">
-                        </svg>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Price
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4"></svg>
                     </p>
                 </th>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Description <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4">
-                        </svg>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Description
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4"></svg>
                     </p>
                 </th>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Quantity <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4">
-                        </svg>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Quantity
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4"></svg>
                     </p>
                 </th>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Date Created <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4">
-                        </svg>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Date Created
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4"></svg>
                     </p>
                 </th>
-                <th
-                    class="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p
-                        class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
-                        Actions</p>
+                <th class="cursor-pointer border border-gray-400 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                    <p class="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                        Actions
+                    </p>
                 </th>
             </tr>
         </thead>
         <tbody>
             @foreach ($products as $product)
                 <tr>
-                    <td class="p-4 border-b border-blue-gray-50">
+                    <td class="p-4 border border-gray-400">
                         <div class="flex items-center gap-3">
                             <div class="flex flex-col">
-                                <p
-                                    class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
-                                    {{ $product->product_name }}</p>
+                                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{ $product->product_name }}</p>
                             </div>
                         </div>
                     </td>
-                    <td class="p-4 border-b border-blue-gray-50">
+                    <td class="p-4 border border-gray-400">
                         <div class="flex items-center gap-3">
                             <div class="flex flex-col">
-                                <p
-                                    class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
-                                    {{ $product->price }}</p>
+                                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{ $product->price }}</p>
                             </div>
                         </div>
                     </td>
-                    <td class="p-4 border-b border-blue-gray-50">
+                    <td class="p-4 border border-gray-400">
                         <div class="flex flex-col">
-                            <p
-                                class="truncate block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
-                                {{ $product->description }}</p>
+                            <p class="truncate block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{ $product->description }}</p>
                         </div>
                     </td>
-                    <td class="p-4 border-b border-blue-gray-50">
+                    <td class="p-4 border border-gray-400">
                         <div class="flex items-center gap-3">
                             <div class="flex flex-col">
-                                <p
-                                    class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
-                                    {{ $product->quantity }}</p>
+                                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{ $product->quantity }}</p>
                             </div>
                         </div>
                     </td>
-                    <td class="p-4 border-b border-blue-gray-50">
-                        <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
-                            {{ $product->created_at }}</p>
+                    <td class="p-4 border border-gray-400">
+                        <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{ $product->created_at }}</p>
                     </td>
 
-                    <td class=" p-4 border-b border-blue-gray-50">
-
-                        <div
-                            class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30">
+                    <td class="p-4 border border-gray-400">
+                        <div class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30">
                             <span class="relative top-2 -left-3 transform -translate-y-1/2 -translate-x-1/2" title="Edit">
                                 <a href="{{ route('admin.edit', ['admin' => $product->id]) }}"><i class="fa-solid fa-pencil text-green-500 text-lg"></i></a>
                             </span>
@@ -110,8 +83,7 @@
                             <form action="{{ route('admin.destroy', ['admin' => $product]) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
-                                    <button class="relative left-6 transform -translate-y-1/2 -translate-x-1/2"
-                                    title="Delete" type="submit"><i class="fa-solid fa-trash text-red-500 text-lg"></i></button>
+                                <button class="relative left-6 transform -translate-y-1/2 -translate-x-1/2" title="Delete" type="submit"><i class="fa-solid fa-trash text-red-500 text-lg"></i></button>
                             </form>
                         </div>
                     </td>
