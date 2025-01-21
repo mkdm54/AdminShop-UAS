decc5a2dfe2d2f7e1ba6c2d79f832861355b818d
Makdum cinta C++ <makdumibrohim3@gmail.com>
2025-01-21 23:57:39 +0700

    tambahkan javascript untuk menu side bar add product

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/css/form.css',
                 'resources/js/app.js',
                 'resources/js/form.js',
-                'resources/js/admin/show_product.js'
+                'resources/js/admin/show_product.js',
+                'resources/js/admin/add_product.js'
             ],
             refresh: true,
         }),
